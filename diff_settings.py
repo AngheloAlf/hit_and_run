12d55f56da91580e32b561daa4bd2903366ff44f
angie <angheloalf95@gmail.com>
2024-03-22 20:00:50 -0300

    Setup asm differ

---
+++ diff_settings.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import argparse
+from pathlib import Path
+
+def add_custom_arguments(parser: argparse.ArgumentParser):
+    version = "us_2003_07_10"
+
+    make_options = Path(".make_options")
+    if make_options.exists():
+        with make_options.open() as f:
+            for line in f:
+                if "VERSION" in line and "=" in line:
+                    version = line.split("=")[1].strip()
+
+    parser.add_argument("-v", "--version", default=version)
+
+def apply(config: dict, args: argparse.Namespace):
+    version = args.version
+
+    config["baseimg"] = f"expected/build/{version}/hit_and_run.{version}.rom"
+    config["myimg"]   = f"build/{version}/hit_and_run.{version}.rom"
+    config["mapfile"] = f"build/{version}/hit_and_run.{version}.map"
+    config["source_directories"] = ["./src", "./include", "./asm", "./lib"]
+    config["objdump_flags"] = ["-Mreg-names=n32"]
+    # config["objdump_flags"].append("-Mno-aliases")
+    config["makeflags"] = ["WERROR=0", f"VERSION={version}", "OBJDUMP_BUILD=0", "PERMUTER=1"]
