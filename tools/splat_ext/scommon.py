64f97bc5f1a079721b9f2bb49b0e403ec8cc708b
Angie <angheloalf95@gmail.com>
2024-03-21 22:52:43 -0300

    setup files

---
+++ tools/splat_ext/scommon.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: © 2024 AngheloAlf
+# SPDX-License-Identifier: MIT
+
+from __future__ import annotations
+
+from splat.segtypes.common.sbss import CommonSegSbss
+
+class PS2SegScommon(CommonSegSbss):
+    def get_linker_section(self) -> str:
+        return ".scommon"
